d1bd0de121d1d61bb91e60c505a4ef964fbd647e
shubhammmenewar <shubhammmenewar@gmail.com>
2026-01-24 15:54:51 +0530

    new Test Case Added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -14,4 +14,38 @@ test('Page Playwright Test', async ({page}) => {
   await page.goto("https://www.google.com/");
   console.log(await page.title()); // to get Title
    await expect(page).toHaveTitle('Google');
+   await page.locator("//textarea[@class='gLFyf']").fill("naukri.com")
+   await page.locator("//div[@class='lnnVSe' and @aria-label='naukri.com']").click();
 });
+
+test('Login RahulShettyacadamy',async({page})=>{
+
+  const username= page.locator('#username');
+  const password= page.locator('#password');
+  const signbtn=  page.locator("#signInBtn");
+
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+console.log(await page.title);
+await page.locator('#username').fill("rahulshetty");
+await password.fill("Learning@830$3mK2");
+await signbtn.click();
+console.log(await page.locator("//div[@style='display: block;']").textContent());
+await expect(page.locator("//div[@style='display: block;']")).toContainText('Incorrect');
+
+} )
+
+test('Verify how to get Single element over multiple found',async({page})=>{
+
+  const username= page.locator('#username');
+  const password= page.locator('#password');
+  const signbtn=  page.locator("#signInBtn");
+
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+console.log(await page.title);
+await page.locator('#username').fill("rahulshetty");
+await password.fill("Learning@830$3mK2");
+await signbtn.click();
+console.log(await page.locator("//div[@style='display: block;']").textContent());
+await expect(page.locator("//div[@style='display: block;']")).toContainText('Incorrect');
+
+} )
